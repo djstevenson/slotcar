454b85851ec423b1d86135d513ddbb4053e6bc6e
David Stevenson <dstevenson@uplandsoftware.com>
2020-01-19 17:49:40 +0000

    Add example client code

---
+++ example/straights.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin::libs;
+use List::Util qw( sum );
+
+# Need a factory to make these easier to construct
+use Slotcar::Track::Straight::Standard;
+use Slotcar::Track::Straight::Half;
+use Slotcar::Track::Straight::StartingGrid;
+use Slotcar::Track::Straight::Quarter;
+use Slotcar::Track::Straight::Short;
+
+my $track = [
+    Slotcar::Track::Straight::Standard->new,
+    Slotcar::Track::Straight::Standard->new,
+    Slotcar::Track::Straight::Half->new,
+    Slotcar::Track::Straight::StartingGrid->new,
+    Slotcar::Track::Straight::Quarter->new,
+    Slotcar::Track::Straight::Short->new,
+];
+
+# Length should be 350x2 + 175x2 + 87.5 + 78mm
+# = 1215.5
+# Working dims are in µm
+# So, expect 1_215_500
+
+my $length = sum map { $_->length } @$track;
+print $length, "\n";
