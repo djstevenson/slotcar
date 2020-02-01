4d580df89b7e570964599caa44df874aebaa43ac
David Stevenson <dstevenson@uplandsoftware.com>
2020-02-01 00:06:31 +0000

    Add piece factory

--- example/straights.pl
+++ example/straights.pl
@@ -6,17 +6,7 @@ use warnings;
 use FindBin::libs;
 use List::Util qw( sum );
 
-# Need a factory to make these easier to construct
-use Slotcar::Track::Straight::ARCPowerBase;
-use Slotcar::Track::Straight::Standard;
-use Slotcar::Track::Straight::Half;
-use Slotcar::Track::Straight::StartingGrid;
-use Slotcar::Track::Straight::Quarter;
-use Slotcar::Track::Straight::Short;
-use Slotcar::Track::Curve::R1::C16;
-use Slotcar::Track::Curve::R2::C8;
-use Slotcar::Track::Curve::R3::C16;
-use Slotcar::Track::Curve::R4::C16;
+use Slotcar::PieceFactory;
 
 use SVG (-inline => 1, -nocredits => 1);
 
@@ -25,22 +15,26 @@ my $svg = SVG->new(  # 1m square base board
     height => 1_000,
 );
 
-my $track = [
-    Slotcar::Track::Straight::ARCPowerBase->new(svg => $svg),
-    Slotcar::Track::Straight::Standard->new(svg => $svg),
-    Slotcar::Track::Straight::Standard->new(svg => $svg),
-    Slotcar::Track::Straight::Half->new(svg => $svg),
-    Slotcar::Track::Straight::StartingGrid->new(svg => $svg),
-    Slotcar::Track::Straight::Quarter->new(svg => $svg),
-    Slotcar::Track::Straight::Short->new(svg => $svg),
-    Slotcar::Track::Curve::R1::C16->new(svg => $svg),
-    Slotcar::Track::Curve::R2::C8->new(svg => $svg),
-    Slotcar::Track::Curve::R3::C16->new(svg => $svg),
-    Slotcar::Track::Curve::R4::C16->new(svg => $svg),
-];
+my $piece_factory = Slotcar::PieceFactory->new(svg => $svg);
+my @track = qw/
+    C8435
+    C8205
+    C8205
+    C8207
+    C7018
+    C8200
+    C8236
+    C8278
+    C8206
+    C8204
+    C8235
+    C8529
+/;
+
+my @pieces = map { $piece_factory->piece($_)} @track;
 
 # Can currently only render one piece
-$track->[1]->render;
+$pieces[1]->render;
 # $track->[5]->render;
 print $svg->xmlify;
 
