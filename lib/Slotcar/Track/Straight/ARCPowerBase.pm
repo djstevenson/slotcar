d3291105ea832be91eba96377d7ea6e9fc79814a
David <david@ytfc.com>
2020-03-15 00:52:03 +0000

    Add finish line to ARC PRO base

--- lib/Slotcar/Track/Straight/ARCPowerBase.pm
+++ lib/Slotcar/Track/Straight/ARCPowerBase.pm
@@ -28,19 +28,67 @@ has '+description' => (default => 'ARC PRO Power Base');
 
 use Readonly;
 
+
+override render_markings => sub {
+    my ($self, $track) = @_;
+
+    $self->_render_checkered_finish_line($track);
+    $self->_render_arc_pro_logo($track);
+};
+
+# Parameters for finish line
+
+Readonly my $FINISH_X_OFFSET   =>  6.0;
+Readonly my $FINISH_Y_OFFSET1  =>  1.0;
+Readonly my $FINISH_Y_OFFSET2  =>  7.0;
+Readonly my $FINISH_Y_OFFSET3  =>  7.0;
+Readonly my $FINISH_BOX_WIDTH  =>  8.0;
+Readonly my $FINISH_BOX_HEIGHT => 16.0;
+
+sub _render_checkered_finish_line {
+    my ($self, $track) = @_;
+
+    my $j1 = $self->joins->{left}->offset_1;
+    my $j2 = $self->joins->{left}->offset_2;
+    my @boxes = (
+        { x => $FINISH_X_OFFSET,                     y => $FINISH_Y_OFFSET1                       },
+        { x => $FINISH_X_OFFSET + $FINISH_BOX_WIDTH, y => $FINISH_Y_OFFSET1  + $FINISH_BOX_HEIGHT },
+
+        { x => $FINISH_X_OFFSET,                     y => $j1 + $FINISH_Y_OFFSET2                       },
+        { x => $FINISH_X_OFFSET + $FINISH_BOX_WIDTH, y => $j1 + $FINISH_Y_OFFSET2  + $FINISH_BOX_HEIGHT },
+        { x => $FINISH_X_OFFSET,                     y => $j1 + 2.0*$FINISH_BOX_HEIGHT+$FINISH_Y_OFFSET2                       },
+        { x => $FINISH_X_OFFSET + $FINISH_BOX_WIDTH, y => $j1 + 2.0*$FINISH_BOX_HEIGHT+$FINISH_Y_OFFSET2  + $FINISH_BOX_HEIGHT },
+
+        { x => $FINISH_X_OFFSET,                     y => $j2 + $FINISH_Y_OFFSET2                       },
+        { x => $FINISH_X_OFFSET + $FINISH_BOX_WIDTH, y => $j2 + $FINISH_Y_OFFSET2  + $FINISH_BOX_HEIGHT },
+    );
+
+    foreach my $box (@boxes) {
+        $track->rectangle(
+            x => $box->{x},
+            y => $box->{y},
+            width => $FINISH_BOX_WIDTH,
+            height => $FINISH_BOX_HEIGHT,
+            fill => $self->white_paint,
+        );
+    }
+
+}
+
+# Paramaters for the 'logo'
 Readonly my $OBLIQUE_X_OFFSET =>  8.0;
 Readonly my $OBLIQUE_HEIGHT   => 38.0;
 Readonly my $STROKE_WIDTH     =>  6.0;
 
-Readonly my @LINE_OFFSETS     => ( 40.0, 53.0, 66.0, 259.0, 272.0, 285.0 );
+Readonly my @LINE_OFFSETS     => ( 50.0, 63.0, 76.0, 269.0, 282.0, 295.0 );
 
-override render_markings => sub {
+sub _render_arc_pro_logo {
     my ($self, $track) = @_;
 
     foreach my $offset (@LINE_OFFSETS) {
         $self->_render_oblique_line($track, $offset);
     }
-};
+}
 
 sub _render_oblique_line {
     my ($self, $track, $offset) = @_;
