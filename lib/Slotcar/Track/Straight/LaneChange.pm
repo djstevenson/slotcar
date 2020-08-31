2378098755af9b951b4237736728bc9201f0af59
David Stevenson <david@ytfc.com>
2020-08-31 01:47:33 +0100

    Implement sensors on straight lane changer with new lane definitions

--- lib/Slotcar/Track/Straight/LaneChange.pm
+++ lib/Slotcar/Track/Straight/LaneChange.pm
@@ -62,17 +62,20 @@ override render_conductor_mods => sub {
     # Real sensors at 'left' end
     # (cars travelling left to right)
 
-    # my $x1 = $X_OFFSET;
-    # my $y1 = $self->joins->{left}->offset_1;
-    # my $y2 = $self->joins->{left}->offset_2;
-    # my @sensors = (
-    #     { x => $x1, y => $y1, type => 'active' },
-    #     { x => $x1, y => $y2, type => 'active' },
-    # );
+    my $x1 = $X_OFFSET;
 
-    # foreach my $sensor ( @sensors ) {
-    #     $self->render_sensor($track, $sensor);
-    # }
+    # Grooves at 1/4 and 3/4 width
+    my $groove_y1 = 1 * $self->lane_offset;
+    my $groove_y2 = 3 * $self->lane_offset;
+
+    my @sensors = (
+        { x => $x1, y => $groove_y1, type => 'active' },
+        { x => $x1, y => $groove_y2, type => 'active' },
+    );
+
+    foreach my $sensor ( @sensors ) {
+        $self->render_sensor($track, $sensor);
+    }
 };
 
 __PACKAGE__->meta->make_immutable;
